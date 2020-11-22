6b99c2972e2252d48990c2f8a77a8c7ae849f4c7
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-22 10:46:05 +0530

    STARTED:snake and ladder simulation

---
+++ SnakeAnDLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAnDLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake And Ladder simulation");
+            Console.ReadKey();
+        }
+    }
+}
